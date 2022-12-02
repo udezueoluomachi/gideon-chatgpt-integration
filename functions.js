fe29bd5f1650f6945becdc83cc3f7a7e12856ecf
udezueoluomachi <74429592+udezueoluomachi@users.noreply.github.com>
2022-12-02 21:24:45 +0100

    added AI completion feature

---
+++ functions.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const _exports = module.exports
+
+_exports.returnBotTypingTimeInSeconds = function (textLength) {
+    let typingSpeed = 56;
+    let typingTimeInMinutes = ((textLength / typingSpeed));
+    return typingTimeInMinutes * 60000
+};
--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 require("dotenv").config();
 const login = require("fca-unofficial");
 const fs = require("fs");
+const {Configuration, OpenAIApi} = require("openai");
+
+//custom library for my personal functions
+const {returnBotTypingTimeInSeconds} = require("./functions.js");
+
+const OPEN_AI_API_KEY = process.env.OPEN_AI_API_KEY;
+
+
+const configuration = new Configuration({
+    apiKey: OPEN_AI_API_KEY
+});
+const openai = new OpenAIApi(configuration);
 
 
 const facebookAccountCrenentials = {
@@ -18,7 +30,25 @@ login(facebookAccountCrenentials , (err, api) => {
             api.markAsRead(event.threadID, (err) => {
                 if(err) return console.log(err);
             });
-            setTimeout(() => api.sendMessage("You said: " + event.body, event.threadID , event.messageID), 3000)
+            if(event.body) {
+                api.sendTypingIndicator(event.threadID, err => {if(err) return console.log(err)})
+                openai.createCompletion({
+                  model: "text-davinci-003",
+                  prompt: event.body,
+                  temperature: 1,
+                  "max_tokens": 2048
+                })
+                .then(response => response.data.choices[0].text)
+                .then(response => {
+                    if(response.length > 0) {
+                        setTimeout(() => api.sendMessage(response, event.threadID , event.messageID), returnBotTypingTimeInSeconds(response.length));
+                    }
+                    else {
+                        setTimeout(() => api.sendMessage(response, event.threadID , event.messageID), 3000);
+                    }
+                })
+                .catch(err => {return console.log(err)})
+            }
         }
     });
 });
