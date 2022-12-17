0544ce16393c6f06da7fc8599d9f947a41456240
udezueoluomachi <74429592+udezueoluomachi@users.noreply.github.com>
2022-12-17 18:49:25 +0100

    using latest version of chatgpt

--- index.js
+++ index.js
@@ -11,36 +11,58 @@ const {
 
 //const {ChatGPTAPI} = await import("chatgpt");
 import("chatgpt")
-.then(({ChatGPTAPI}) => {
-    const _chatgptInstance = new ChatGPTAPI({
-        sessionToken : process.env.SESSION_TOKEN
+.then(({ChatGPTAPI, getOpenAIAuth}) => {
+    
+    getOpenAIAuth({
+        email: process.env.OPENAI_EMAIL,
+        password: process.env.OPENAI_PASSWORD
     })
-    _chatgptInstance.ensureAuth().then(()=> {
-        console.log("authenticated");
-    
-    
-        const facebookAccountCrenentials = {
-            appState: JSON.parse(fs.readFileSync('fbstate.json', 'utf8'))
-        }
-        login(facebookAccountCrenentials , (err, api) => {
-            if(err) return console.error(err);
-            console.log("Bot online\nHello, my name is Gideon")
-            api.setOptions({listenEvents: true});
+    .then((openAIAuth) => {
+        const _chatgptInstance = new ChatGPTAPI({...openAIAuth})
+        _chatgptInstance.ensureAuth().then(()=> {
+            console.log("authenticated");
         
-            var listenEmitter = api.listen((err, event) => {
+        
+            const facebookAccountCrenentials = {
+                appState: JSON.parse(fs.readFileSync('fbstate.json', 'utf8'))
+            }
+            login(facebookAccountCrenentials , (err, api) => {
                 if(err) return console.error(err);
-                
-                if (event.type === "message" || event.type === "message_reply") {
-                    api.markAsRead(event.threadID,true , (err) => {
-                        if(err) return console.log(err);
-                    });
-                    if(event.body) {
-                        if(event.isGroup) {
-                            checkIfBotName(event.body)
-                            .then(message => {
+                console.log("Bot online\nHello, my name is Gideon")
+                api.setOptions({listenEvents: true});
+            
+                var listenEmitter = api.listen((err, event) => {
+                    if(err) return console.error(err);
+                    
+                    if (event.type === "message" || event.type === "message_reply") {
+                        api.markAsRead(event.threadID,true , (err) => {
+                            if(err) return console.log(err);
+                        });
+                        if(event.body) {
+                            if(event.isGroup) {
+                                checkIfBotName(event.body)
+                                .then(message => {
+                                    //AI response
+                                    _chatgptInstance.sendMessage(message)
+                                    .then(response => {
+                                        api.sendTypingIndicator(event.threadID, err => {if(err) return console.log(err)})
+                                        if(response.length > 0) {
+                                            setTimeout(() => api.sendMessage(response, event.threadID , event.messageID), returnBotTypingTimeInSeconds(response.length));
+                                        }
+                                        else {
+                                            setTimeout(() => api.sendMessage(response, event.threadID , event.messageID), 3000);
+                                        }
+                                    })
+                                    .catch(err => {
+                                        setTimeout(() => api.sendMessage("Unfortunately, an error occured", event.threadID , event.messageID), 3000);
+                                        return console.log(err)
+                                    })
+                                })
+                                .catch(err => {return console.log(err)})
+                            }
+                            else {
                                 //AI response
-                                _chatgptInstance.sendMessage(message)
-                                .then(response => {
+                                _chatgptInstance.sendMessage(event.body).then(response => {
                                     api.sendTypingIndicator(event.threadID, err => {if(err) return console.log(err)})
                                     if(response.length > 0) {
                                         setTimeout(() => api.sendMessage(response, event.threadID , event.messageID), returnBotTypingTimeInSeconds(response.length));
@@ -53,29 +75,12 @@ import("chatgpt")
                                     setTimeout(() => api.sendMessage("Unfortunately, an error occured", event.threadID , event.messageID), 3000);
                                     return console.log(err)
                                 })
-                            })
-                            .catch(err => {return console.log(err)})
-                        }
-                        else {
-                            //AI response
-                            _chatgptInstance.sendMessage(event.body).then(response => {
-                                api.sendTypingIndicator(event.threadID, err => {if(err) return console.log(err)})
-                                if(response.length > 0) {
-                                    setTimeout(() => api.sendMessage(response, event.threadID , event.messageID), returnBotTypingTimeInSeconds(response.length));
-                                }
-                                else {
-                                    setTimeout(() => api.sendMessage(response, event.threadID , event.messageID), 3000);
-                                }
-                            })
-                            .catch(err => {
-                                setTimeout(() => api.sendMessage("Unfortunately, an error occured", event.threadID , event.messageID), 3000);
-                                return console.log(err)
-                            })
+                            }
                         }
                     }
-                }
+                });
             });
-        });
-    }).catch(err => console.log(err))    
+        }).catch(err => console.log(err))
+    }).catch(err => console.log("Failed to getopenAiAuth, \n%s", err))
 })
 .catch(err => console.log(err))
